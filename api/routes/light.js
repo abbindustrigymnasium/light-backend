2f7335709b26667af5ee8ee1f9f5c4bf883e9fd0
Joakim Flink <39080802+abbjoafli@users.noreply.github.com>
2018-11-20 15:35:17 +0100

    Initial commit

---
+++ api/routes/light.js
@@ -0,0 +1,133 @@
+const express = require('express');
+const  router = express();
+var mysql = require("mysql");
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "jocke",
+  password: "xbox",
+  database: "light"
+});
+
+
+con.connect(function (err) {
+    if (err) throw err;
+});
+
+
+var Values_fromDB;
+var CronJob = require('cron').CronJob;
+new CronJob('* * * * * *', function() {
+  
+    var GetLight = function () {
+        return new Promise(function (resolve, reject) {
+            con.query("SELECT * FROM switch", function (err, result) {
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(result);
+                }
+
+            });
+        });
+    }
+    GetLight().then(response =>{
+        Values_fromDB= response;
+        //console.log(Values_fromDB);
+    })
+}, null, true, 'America/Los_Angeles');
+
+
+
+
+router.get('/:lampName', (req, res) => {
+    var found=false;
+    var Outputvalue;
+    Values_fromDB.forEach(element => {
+        if (element.name== req.params.lampName) {
+            found=true;   
+            Outputvalue =element;
+        
+        }
+    });
+    if (found!= true) {
+    res.status(200).json({name: "none",
+    message: "No such lamp exists"});
+    }
+    else
+    {
+        res.status(200).json(Outputvalue);
+        console.log(Outputvalue);
+    }
+    });
+
+router.get('/', (req, res, next) => {
+
+    res.status(200).json(Values_fromDB);
+});
+
+router.post('/', (req, res) => { //  '/'= indexfilen i localhost/products/   req = request , res= resultat , next = det som ska hända senare 
+
+    Lights = [req.body.name, req.body.hard, req.body.strength];
+    console.log(req.body); //Här skriver jag ut req.body för att se skillnaden på den mellan en postman request och en html.
+    var createProduct = function () { //Skapar funktionen
+        return new Promise(function (resolve, reject) { //Skapar löftet
+            con.query('INSERT INTO switch (name, hard,strength) VALUES ?', [[Lights]], function (err, result) {
+
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(Lights);
+                }
+
+            }); // query
+        }); // Promise
+    } // getDepartments
+
+    createProduct().then(Theproduct => {
+        res.status(200).json({ //Här kan man som jag gjort lägga till en status 200, OBS tänk på att 201 kommer
+            message: "Success, new light logged" //I detta exempel skickar 200 bara produkten medans 201 skickar meddelandet med.
+        });
+        //Tog bort res.status(201) då dubbla status skickningar skapar error, vill man ha möjlighet till bägge så gör if else satser och lägg dem i.
+
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+});
+
+router.patch('/', (req, res) => { //  '/'= indexfilen i localhost/products/   req = request , res= resultat , next = det som ska hända senare 
+    Lights = [req.body.hard, req.body.strength, req.body.name];
+    console.log(req.body); //Här skriver jag ut req.body för att se skillnaden på den mellan en postman request och en html.
+    var createProduct = function () { //Skapar funktionen
+        return new Promise(function (resolve, reject) { //Skapar löftet
+            con.query('UPDATE switch SET `hard` = ? , `strength` = ? Where `name` = ?', [Lights[0], Lights[1], Lights[2]], function (err, result) {
+console.log(result);
+                if (err) {
+                    return reject(err);
+                } else {
+                    return resolve(Lights);
+                }
+
+            }); // query
+        }); // Promise
+    } // getDepartments
+
+    createProduct().then(Theproduct => {
+        res.status(200).json({ //Här kan man som jag gjort lägga till en status 200, OBS tänk på att 201 kommer
+            message: "Success, light updated" //I detta exempel skickar 200 bara produkten medans 201 skickar meddelandet med.
+        });
+
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
+})
+
+module.exports = router;
+
+
+
---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const lightRoutes= require('./api/routes/light');
+
+var cors = require('cors');
+app.use((req, res, next) => {
+       // Website you wish to allow to connect
+       res.setHeader('Access-Control-Allow-Origin', '*');
+
+       // Request methods you wish to allow
+       res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+   
+       // Request headers you wish to allow
+       //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+       res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+       // Set to true if you need the website to include cookies in the requests sent
+       // to the API (e.g. in case you use sessions)
+       res.setHeader('Access-Control-Allow-Credentials', true);
+   
+       // Pass to next layer of middleware
+   
+    next();
+});
+
+app.use(morgan('dev'));
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+app.use('/light', lightRoutes);
+
+
+// app.use((req, res, next) => {
+//     const error = new Error('Lightswitch');
+//     error.status= 404;
+//     next(error);
+// });
+
+app.use((error,req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message: error.message
+        }
+    });
+});
+
+module.exports= app;
