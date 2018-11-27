ed9f72c76aa9064c37d4f94969c10dbeb895fbec
Joakim Flink <39080802+abbjoafli@users.noreply.github.com>
2018-11-27 10:16:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const app = require('./app');
-const port= process.env.PORT || 5000;
+const port= process.env.PORT || 3001;
 
 const server = http.createServer(app);
 
