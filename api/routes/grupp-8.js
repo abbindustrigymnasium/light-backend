8fe26015ca2e9072a4318d5fd1e5be49f5157ed5
abbnilbor <43879825+nilbor@users.noreply.github.com>
2018-12-10 08:22:29 +0100

    Tillagd funktion som tar emot requests från google home

--- api/routes/grupp-8.js
+++ api/routes/grupp-8.js
@@ -150,5 +150,44 @@ router.patch('/:lampName', (req, res, next) => {
     });
 });
 
+router.post('/:google_home/:lampName', (req, res, next) => {
+    const light = {
+        onoff: req.params.onoff,
+    }
+
+    var updateLight = function(){
+        return new Promise(function(resolve, reject){
+
+            con.query('UPDATE `lightstatus` SET `onoff`= ? WHERE `name` = ?',[light.onoff, req.params.lampName], function (error, results) {
+                if (error) 
+                return reject (error);
+                else
+                return resolve(results)
+            });
+        
+        } )
+
+    }
+
+    updateLight().then(result => {
+
+   if (result.affectedRows!=0) {
+        
+        res.status(200).json(result);
+    
+    }
+    
+    else
+    res.status(404).json({
+        message: "Update imposible, lack of values"
+        });
+
+    }   ).catch(error => {
+        res.status(500).json({
+            error: error
+        })
+    });
+});
+
 
 module.exports = router;
