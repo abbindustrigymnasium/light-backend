05071e4120f67800269acf6f57d9c1f45e8ba1e4
ABBjaceng <43879993+ABBjaceng@users.noreply.github.com>
2018-12-11 14:16:56 +0100

    Update grupp9.js

--- api/routes/grupp9.js
+++ api/routes/grupp9.js
@@ -35,15 +35,16 @@ router.post('/', (req, res, next) => {
     const Lampa = {
         ID: req.body.ID,
         Temperatur: req.body.Temperatur,
-        Ljusstyrka: req.body.Ljusstyrka
+        Ljusstyrka: req.body.Ljusstyrka,
+        Mode: reqbody.Mode
     };
     
 var createLampa = function(){
     return new Promise(function(resolve, reject){
         
-        var TheLampa = [Lampa.ID, Lampa.Temperatur, Lampa.Ljusstyrka];
+        var TheLampa = [Lampa.ID, Lampa.Temperatur, Lampa.Ljusstyrka, Lampa.Mode];
         console.log(TheLampa);
-        con.query('INSERT INTO lampa (ID, Temperatur, Ljusstyrka) VALUES ?', [[TheLampa]], function(error, results, fields) {
+        con.query('INSERT INTO lampa (ID, Temperatur, Ljusstyrka, Mode) VALUES ?', [[TheLampa]], function(error, results, fields) {
             if (error)
             return reject (error);
             else 
@@ -82,7 +83,7 @@ router.patch('/', (req, res, next) => {
     var updateproduct = function(){
         return new Promise(function(resolve, reject){
             
-            con.query('UPDATE `lampa` SET `Temperatur`=?, `Ljusstyrka`=? WHERE ID = ?', [ lampa.Temperatur, lampa.Ljusstyrka, lampa.ID], function(error, results, fields) {
+            con.query('UPDATE `lampa` SET `Temperatur`=?, `Ljusstyrka`=?, `Mode`=? WHERE ID = ?', [ lampa.Temperatur, lampa.Ljusstyrka, lampa.Mode, lampa.ID], function(error, results, fields) {
                 if (error)
                 return reject (error);
                 else 
